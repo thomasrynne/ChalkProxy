b129eea487d68262a3ed8ea62b28d54d556aa07b
Thomas Rynne <github@thomasrynne.co.uk>
2012-10-05 22:04:53 +0100

    Adds GPL license

--- src/chalkproxy/ChalkProxy.scala
+++ src/chalkproxy/ChalkProxy.scala
@@ -1,3 +1,20 @@
+/* 
+    ChalkProxy - a directory for a team's test web servers
+    Copyright (C) 2012 Thomas Rynne
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    See http://www.gnu.org/copyleft/gpl.html
+
+*/
 package chalkproxy
 
 import java.net.URI
